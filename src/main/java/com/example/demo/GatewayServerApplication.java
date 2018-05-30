a45213999b11d86c2ac7692a24f84a9a31028416
jw88 <jw88@live.com>
2018-05-30 11:29:10 -0700

    u

--- src/main/java/com/example/demo/GatewayServerApplication.java
+++ src/main/java/com/example/demo/GatewayServerApplication.java
@@ -2,11 +2,19 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class GatewayServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
 }
+
+//http://localhost:8765/{application-name [from eureka]}/{uri}
+//http://localhost:8765/currency-exchange-service/currency-exchange/from/USD/to/CNY
+//http://localhost:8765/currency-conversion-service/currency-converter-feign/from/EUR/to/CNY/quantity/120
